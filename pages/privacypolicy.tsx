c1872f6422319bb8ed9ed21d3db5430f78fc8cc5
Zack Griffin <40415631+griffin2k@users.noreply.github.com>
2022-02-01 16:47:32 -0800

    Privacy Policy

--- pages/privacypolicy.tsx
+++ pages/privacypolicy.tsx
@@ -16,30 +16,22 @@ export default function PrivacyPolicy() {
                     We automatically collect certain information when you visit, use or navigate the Site. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our and other technical information. This information is primarily needed to maintain the security and operation of our Site, and for our internal analytics and reporting purposes.
                     <br></br>
                     <br></br>
-                    <b className="text-lg">2. COOKIES</b>
-                    <br></br>
-                    Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device&apos;s internal memory.
-                    <br></br>
-                    <br></br>
-                    This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
-                    <br></br>
-                    <br></br>
-                    <b className="text-lg">3. THIRD-PARTY SITES</b>
+                    <b className="text-lg">2. THIRD-PARTY SITES</b>
                     <br></br>
                     This Private Policy applies only to the Services. The Services may contain links to other websites not operated or controlled by us (the “Third Party Sites”). The policies and procedures we described here do not apply to the Third Party Sites. The links from the services do not imply that we endorse or have reviewed the Third Party Sites. We suggest contacting those sites directly for information on their privacy policies.
                     <br></br>
                     <br></br>
-                    <b className="text-lg">SECURITY</b>
+                    <b className="text-lg">3. SECURITY</b>
                     <br></br>
                     AvaAssistant takes reasonable measures to protect your information from unauthorized access or against loss, misuse or alteration by third parties. Although we make good faith efforts to store the information collected on the Service in a secure operating environment that is not available to the public, we cannot guarantee the absolute security of that information during its transmission or its storage on our systems. Further, while we attempt to ensure the integrity and security of our network and systems, we cannot guarantee that our security measures will prevent third-party “hackers” from illegally obtaining access to this information. We do not warrant or represent that your information will be protected against, loss, misuse, or alteration by third parties. No method of transmission over the Internet, or method of electronic storage, is 100% secure, however. Therefore, we cannot guarantee its absolute security.
                     <br></br>
                     <br></br>
-                    <b className="text-lg">CHANGES TO THIS POLICY</b>
+                    <b className="text-lg">4. CHANGES TO THIS POLICY</b>
                     <br></br>
                     We reserve the right to update or modify this Privacy Policy at any time and from time to time without prior notice. Please review this policy periodically, and especially before you provide any information. This Privacy Policy was last updated on the date indicated above.
                     <br></br>
                     <br></br>
-                    <b className="text-lg">CONTACTING US</b>
+                    <b className="text-lg">5. CONTACTING US</b>
                     <br></br>
                     Please feel free to contact us if you have any questions about this Private Policy or the information practices of the Service. You may contact us at: support@avaassistant.xyz
                 </p>
